26d2ed555c1d3604f8f4bd060f08902bb5818125
mibarra18 <51798451+mibarra18@users.noreply.github.com>
2021-07-27 17:43:29 -0500

    Delete Tienda/Apps/GestionProductos directory

--- Tienda/Apps/GestionProductos/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from Tienda.Apps.GestionProductos.models import *
-
-# Register your models here.
-
-admin.site.register(Producto)
-admin.site.register(Estante)
-admin.site.register(Registro)
--- Tienda/Apps/GestionProductos/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class GestionproductosConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Tienda.Apps.GestionProductos'
