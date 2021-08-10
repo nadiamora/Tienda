557778c849f79ddb17bb46e8be535067c0fc0222
jemmanuel2410 <83848000+jemmanuel2410@users.noreply.github.com>
2021-08-10 16:33:13 -0500

    Update apps.py

--- Tienda/Apps/GestionProductos/apps.py
+++ Tienda/Apps/GestionProductos/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class GestionproductosConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Tienda.Apps.GestionProductos'
+#Tienda/Apps/GestionProductos
