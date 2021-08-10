b106b64299f086adc2bf32c6861dc27b5207960d
mibarra18 <51798451+mibarra18@users.noreply.github.com>
2021-08-10 14:28:17 -0500

    Update views.py

--- Tienda/Apps/GestionProductos/views.py
+++ Tienda/Apps/GestionProductos/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-#En esta pestaña se agregan las vistas de cada aplicacion
+# En esta pestaña se agregan las vistas de cada aplicacion.
+# El crud esta en la pestaña de admin.
